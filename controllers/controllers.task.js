b3c7917177e31e57c083096556f4c5ba81b97861
Rajansharma995 <rajan.kandel.662@gmail.com>
2023-09-01 22:35:51 +0545

    something

--- controllers/controllers.task.js
+++ controllers/controllers.task.js
@@ -32,12 +32,34 @@ const getItems= async (req,res,next) =>{
 }
 
 
-const updateItems= (req,res) =>{
-    res.json('update items');
+
+
+
+const deleteItems= async (req,res,next) =>{
+    try{
+        const {id: itemsID} = req.params
+        const items = await Items.findOneAndDelete({_id: itemsID});
+        if (!items ){
+            return res.status(404).json({message:`No such id : ${itemsID}`})
+        }
+         res.status(200).json(items);
+        }catch(e){
+               next(e);
+        }
+}
+
+const updateItems= async (req,res,next) => {
+    try  {
+        const {id: itemsID} =req.params;
+        const items = await Items.findOneAndUpdate({_id: itemsID},req.body,{
+            new:true , runValidators: true 
+        });
+        if (!items )return res.status(404).send(`Items with the given ID not found`);
+        res.status(200).json({items});
+        } catch (e) {
+            next(e);
+            };
 }
 
 
-const deleteItems= (req,res) =>{
-    res.json('delete items');
-}
 module.exports = {getAllItems , createItems  ,getItems   ,updateItems    ,deleteItems } ;
